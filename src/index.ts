5149439a8f69268432342e235a0c87a0514ad473
Pacheco333 <santiago2006ortizp@gmail.com>
2025-10-22 00:46:33 -0500

    Backend adaptado

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import asesorRoutes from './modules/asesor/asesor.routes';
+
+const app = express();
+const PORT = 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rutas por módulos/roles
+
+app.use('/api/asesor', asesorRoutes);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.json({ 
+    message: 'API Banca Uno - Sistema Bancario',
+    modules: {
+      cajero: '/api/cajero/*',
+      asesor: '/api/asesor/*'
+    }
+  });
+});
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(` Servidor corriendo en http://localhost:${PORT}`);
+  console.log(` Base de datos: banca_uno`);
+ 
+});
