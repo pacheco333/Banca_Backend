38d231fc1b7f643667c9c01de5ed9ded4533bbff
Pacheco333 <santiago2006ortizp@gmail.com>
2025-11-24 13:03:07 -0500

    Configuracion de base de datos con credenciales aiven

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: 'sql204.infinityfree.com',
+  host: 'banca-uno-santiago2006ortizp-5f86.b.aivencloud.com',
   user: 'if0_40491480',          
-  password: 'N41fdfo0V1NqI',           
-  database: 'if0_40491480_banca_uno',
-  //port: 3306,
+  password: '',           
+  database: 'defaultdb',
+  port: 13730,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
